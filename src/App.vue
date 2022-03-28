604e133aa842739092538be1bfccf06ce48a2c5f
alicifes <635023574@qq.com>
2022-03-28 16:33:37 +0800

    修改了一些基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-  <div>hi</div>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
