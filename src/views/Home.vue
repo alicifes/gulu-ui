c7967dc5c2cd94d522d8d071797bd88c731f0901
alicifes <635023574@qq.com>
2022-03-28 17:41:33 +0800

    初始化html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>轱辘UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -21,5 +21,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.banner{
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #97e49b;
+  >.actions{
+    padding: 8px 0;
+    a{
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+
+}
 
 </style>
