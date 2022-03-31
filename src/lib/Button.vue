092f4c869a43df4d0a2c74d49b9f5a36b69dfa61
alicifes <635023574@qq.com>
2022-03-31 17:31:41 +0800

    css最小影响原则

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="gulu-button" :class="`theme-${theme}`" >
+  <button class="gulu-button" :class="`gulu-theme-${theme}`" >
     <slot/>
   </button>
 </template>
@@ -18,7 +18,39 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-div {
+<style lang="scss">
+$h:32px;
+$border-color:#d9d9d9;
+$color:#333;
+$blue:#40a9ff;
+$radius:4px;
+.gulu-button{
+  box-sizing:  border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background-color: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black,0.95);
+  & + & { //当两个并排的时候
+   margin-left: 8px;
+  }
+  &:focus,:hover{
+   color: $blue;
+    border-color: $blue;
+  }
+  &:focus{
+    outline: none;
+  }
+  &::-moz-focus-inner{
+    border:0;   //在Firefox中使用
+  }
 }
+
 </style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="{checked:value}" @click="toggle"><span></span></button>
+  <button :class="{'gulu-checked':value}" @click="toggle" class="gulu-switch"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -17,11 +17,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
 
-button {
+.gulu-switch{
   height: $h;
   width: $h*2;
   position: relative;
@@ -38,7 +38,7 @@ button {
     border-radius: $h2/2;
     transition:all 0.5s;
   }
-  &.checked{
+  &.gulu-checked{
     background: #1890ff;
     >span{
       left: calc(100% - #{$h2} - 2px);
