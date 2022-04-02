ee3d311b992a667c41d84a3f582eb0360dd47ee4
alicifes <635023574@qq.com>
2022-04-02 11:18:39 +0800

    完成了dialong界面的编写

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    DialogDemo
+    <div>dialog示例</div>
+    <h1>示例1</h1>
+    <Dialog/>
   </div>
 </template>
 
 <script>
+import Dialog from "../lib/Dialog.vue";
+
 export default {
-  name: "Dualog"
+  components: {Dialog}
 }
 </script>
 
