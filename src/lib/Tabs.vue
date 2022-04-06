f9b9d0ab8e9073d7e51f55680745ddfc53f590ba
alicifes <635023574@qq.com>
2022-04-06 20:55:50 +0800

    使用css实现切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,85 @@
 <template>
-  <div>
-
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item" :class="{selected:t===selected}" v-for="(t,index) in titles" :key="index"
+           @click="select(t)">{{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c"
+                 :class="{selected:c.props.title === selected}" :key="index"/>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from '../lib/Tab.vue';
+import {computed} from 'vue';
+
 export default {
-  name: "Tabs"
-}
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw Error('内部的组件必须为Tab');
+      }
+    });
+    const titles = defaults.map((tags) => {
+      return tags.props.title
+    });
+    const current= computed(()=>{
+       return defaults.filter((tag)=>{
+        return tag.props.title = props.selected
+      })[0]
+    });
+    const select = (tags:string) => {
+      context.emit('update:selected', tags);
+    };
+    return {defaults, titles,current, select};
+  }
+};
 </script>
 
-<style scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
 
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+
+    &-item {
+      display: none;
+
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
